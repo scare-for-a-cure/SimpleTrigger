5bbc546604489ce7f1ce233ff382d38cda36e875
JamesM92 <36548759+JamesM92@users.noreply.github.com>
2021-10-16 12:27:35 -0500

    Update SimpleTrig.ino

--- SimpleTrig/SimpleTrig.ino
+++ SimpleTrig/SimpleTrig.ino
@@ -1,15 +1,12 @@
 /*
 This is a simpe trigger program, meant to read a sensor and start a signal for a given amount of time.
 
-this is to replace using a picoboo just to trigger a timed event
+this is to replace using a pico-boo just to trigger a timed event
 
 
 Developed by: James Manley
 
 */
-
-
-
 
 //LIBRARIES
 #include <RBD_Timer.h>  // https://github.com/alextaujenis/RBD_Timer
@@ -27,13 +24,15 @@ RBD::Timer ch1_Time(5000);
 //////////////////////////////////////////////////////////
 /// V leave below code alone V ///
 
+//inputs
+// the input button is being defined within the setup loop due to changes in how it is handled bnased on the configuration settings.
 
 //outputs
+int LED_Armed = 13 ; // define the built in LED as the arming led for ststua updates
 int ch_1 = 2; // define channel 1 pin
 
-//inputs
-//int trigger = 11 ; 
-
+//global variables
+int ledState = LOW;
 
 
 
@@ -45,13 +44,12 @@ void setup(){
   digitalWrite(ch_1, !ttl);
   ch1_Time.onExpired();
   
-  pinMode(trigger, INPUT);
   if(sig == HIGH){   /// the button event by default is looking for a pull to ground signal, this will changer it to a pull to high.
     RBD::Button trig(11, false); // button is defined here witht he internal pull up turned off for a high signal
     trig.invertReading();
   }
   else{
-    RBD::Button trig(11, false); // if button is the normal pull to ground, then call it as normal
+    RBD::Button trig(11); // if button is the normal pull to ground, then call it as normal
   }
   
   standby.restart();
@@ -61,8 +59,22 @@ void setup(){
 
 
 void loop(){
-//  if(standby.isExpired() && digitalRead(trigger)){
-    if(standby.isExpired() && trig.onPressed()){
+   if(standby.isActive()){ // this script handles the flashing of the armed led to go progressively faster as it gets ready to arm.
+    led.setTimeout( map(standby.getPercentValue(), 0, 100, 1000, 50)) ;
+    if(led.onRestart()){
+      ledState = !ledState;
+      digitalWrite(LED_Armed, ledState);
+    }
+  }
+  
+  if(standby.onExpired()){
+    Serial.println("Standby time has elapsed");
+    digitalWrite(LED_Armed, HIGH);
+  } 
+ 
+  
+  if(standby.isExpired() && trig.onPressed()){
+    Serial.println("Button pressed, sequence started");
     digitalWrite(ch_1, ttl);
     ch1_Time.restart();
   }
